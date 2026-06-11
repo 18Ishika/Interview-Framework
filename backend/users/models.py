7e894fdcdbf5a39ba2a2056057dc338ce6271f27
Sonu Kumar <deepakkumar23887@gmail.com>
2026-06-11 17:56:00 +0530

    remove methods

--- backend/users/models.py
+++ backend/users/models.py
@@ -23,13 +23,3 @@ class User(models.Model):
 
     def __str__(self):
         return f"{self.clerk_user_id} ({self.role})"
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
-
-    def __str__(self):
-        return f"{self.clerk_user_id} ({self.role})"
