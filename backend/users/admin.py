3b9d6db14ceb065f2af4071ced33a43633d85735
Sonu Kumar <deepakkumar23887@gmail.com>
2026-06-12 19:31:07 +0530

    add migrations and register models

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Experience, Education
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Education)
+admin.site.register(Experience)
