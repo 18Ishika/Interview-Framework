695fd7ebbb06bdaa0e90f673455c2a880d166267
Sonu Kumar <deepakkumar23887@gmail.com>
2026-06-12 19:16:04 +0530

    task 1 done

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,9 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets, status
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import UserSerializer
-from .models import User
 from .serializers import UserSerializer
 from .utils.api_response import (
     success_response,
