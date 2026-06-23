3b51b5308382b91016a3f772305f42cbfc522be2
Sonu Kumar <deepakkumar23887@gmail.com>
2026-06-23 12:51:07 +0530

    create profile details api & show details on profile page

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import sync_user, me , profile
+from .views import sync_user, me, profile, get_profile_details
 
 
 urlpatterns = [
     path("sync/", sync_user, name="sync-user"),
     path("me/", me, name="me"),
     path("profile/", profile, name="profile"),
+    path("details/", get_profile_details, name="profile-details"),
 ]
