d7e08fbc4d77464e8a2371b43a5d2183ed6b369d
18Ishika <ishika.datascientist@gmail.com>
2026-07-02 17:57:47 +0530

    setup for celery and redis

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -120,3 +120,11 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = True  # temporary for development
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# --- Celery ---
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
